78c9fd0e66cfddbbdf63e9c10a45e996882ed359
Artur Novikov <art14ek@tut.by>
2023-10-25 22:02:29 +0300

    react-hook-form integrated with signUp

--- src/pages/DesktopSignUp/signUp.js
+++ src/pages/DesktopSignUp/signUp.js
@@ -1,51 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 import styles from './signUp.module.scss';
 
 const SignUp = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    agree: false,
-  });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleCheckboxChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      agree: e.target.checked,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: Implement the submit logic here
-    console.log(formData);
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <div className={styles.signUpContainer}>
       <h1>Create new account</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Username</label>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          {...register('username', { required: true, minLength: 3, maxLength: 20 })}
+        />
+        {errors.username && <p>Username should be between 3 and 20 characters.</p>}
         <label>Email address</label>
-        <input type="email" name="email" placeholder="Email address" onChange={handleChange} />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email address"
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>Email is required and should be valid.</p>}
         <label>Password</label>
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          {...register('password', { required: true, minLength: 6, maxLength: 40 })}
+        />
+        {errors.password && <p>Password should be between 6 and 40 characters.</p>}
         <label>Repeat Password</label>
-        <input type="password" name="confirmPassword" placeholder="Repeat Password" onChange={handleChange} />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Repeat Password"
+          {...register('confirmPassword', { validate: (value) => value === watch('password') })}
+        />
+        {errors.confirmPassword && <p>Passwords should match.</p>}
         <div className={styles.agreeCheckbox}>
-          <input type="checkbox" name="agree" onChange={handleCheckboxChange} />
+          <input type="checkbox" name="agree" {...register('checkbox', { required: true })} />
+          {errors.agree && <p>You must agree before submitting.</p>}
           <label>I agree to the processing of my personal information</label>
         </div>
         <button type="submit">Create</button>
